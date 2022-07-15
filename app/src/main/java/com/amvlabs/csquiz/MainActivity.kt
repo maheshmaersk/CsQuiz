75001553425b55ad2dded1359334fdbd039a63c3
Mahesh <70198087+maheshmaersk@users.noreply.github.com>
2022-07-15 21:07:15 +0530

    Spinner
    CheckBox
    
    RadioButton
    
    Dynamic Check box

--- app/src/main/java/com/amvlabs/csquiz/MainActivity.kt
+++ app/src/main/java/com/amvlabs/csquiz/MainActivity.kt
@@ -1,24 +1,76 @@
 package com.amvlabs.csquiz
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.*
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-//    lateinit var mContext: Context
+        lateinit var mContext: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        mContext= applicationContext
-
-        val loginButton = findViewById<Button>(R.id.btLogin)
+        mContext= applicationContext
+        val checkBo = findViewById<AppCompatCheckBox>(R.id.cbAcceptTerms)
+        val rbMale = findViewById<AppCompatRadioButton>(R.id.rbMale)
+        val rbFeMale = findViewById<AppCompatRadioButton>(R.id.rbFeMale)
+        val loginButton = findViewById<AppCompatEditText>(R.id.btLogin)
+        val genderButton = findViewById<AppCompatButton>(R.id.btGender)
+        val llddd = findViewById<LinearLayoutCompat>(R.id.llDynamicCheck)
+        val countries = findViewById<AppCompatSpinner>(R.id.spCountries)
         loginButton.setOnClickListener {
-            Log.d("TestLogs","Login Bt Click")
+            Log.d("TestLogs", "Login Bt Click")
             startActivity(Intent(it.context, OnBoardingActivity::class.java))
         }
 
+        genderButton.setOnClickListener {
+            var das = if (rbMale.isChecked) {
+                "male"
+            } else if(rbFeMale.isChecked) {
+                "Female"
+            } else {
+                "N/A"
+            }
+            Log.e("GenderLogs", das)
+        }
+
+
+        var correctNam = CheckBox(this)
+        var inCorrectNam = CheckBox(this)
+        var wrongNam = CheckBox(this)
+
+        llddd.layoutParams = LinearLayoutCompat.LayoutParams(LinearLayoutCompat.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+        correctNam.text = "Correct Name"
+        inCorrectNam.text = "InCorrect Name"
+        wrongNam.text = "Wrong Name"
+        llddd.addView(correctNam)
+        llddd.addView(inCorrectNam)
+        llddd.addView(wrongNam)
+
+        checkBo.setOnCheckedChangeListener { compoundButton, isChecked ->
+            Log.e("MainCb", "CheckBox Clicked $isChecked")
+        }
+
+        val stateList = arrayListOf<String>("Ka","Tn","MH","AP","TS")
+//
+        val mAdapter = ArrayAdapter(mContext,android.R.layout.simple_spinner_dropdown_item,stateList)
+        countries.adapter= mAdapter
+
+        countries.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Snackbar.make(view!!, "${parent!!.getItemAtPosition(position)}", Snackbar.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Snackbar.make(parent!!.rootView, "Nothing Selected", Snackbar.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
